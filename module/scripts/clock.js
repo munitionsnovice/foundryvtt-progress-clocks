dd2fc257fbf05310fc908872d099c099976c491d
Matt Dawson <mattdawson@WTMBP-Matt-Dawson.local>
2022-07-14 18:29:42 -0400

    Refactor base Clock and ClockSheet

--- module/scripts/clock.js
+++ module/scripts/clock.js
@@ -1,111 +1,87 @@
 const nextIndexInArray = (arr, el) => {
-  const idx = arr.indexOf(el);
-  return (idx < 0 || idx >= arr.length) ? 0 : idx + 1;
+    const idx = arr.indexOf(el);
+    return (idx < 0 || idx >= arr.length) ? 0 : idx + 1;
 }
 
 export class Clock {
-    static get sizes () {
-        return [2, 3, 4, 5, 6, 8, 10, 12];
-    }
+    static sizes = [
+        2, 3, 4, 5, 6, 8, 10, 12
+    ];
 
-    static get themes () {
-        return this._themes;
-    }
+    static image = {
+        width: 350,
+        height: 350
+    };
+
+    static themePath = 'modules/progress-clocks/themes';
+
+    static themes = {
+        gms_red: `${Clock.themePath}/gms_red`,
+        gms_red_grey: `${Clock.themePath}/gms_red_grey`,
+        wallflower_green: `${Clock.themePath}/wallflower_green`,
+        wallflower_green_grey: `${Clock.themePath}/wallflower_green_grey`
+    };
 
     constructor ({ theme, size, progress } = {}) {
-        this.themesPromise = FilePicker.browse("data", "modules/progress-clocks/themes").then(data => {
-            let tempDirs = data.dirs;
-            let newDirs = [];
-            let newPaths = [];
-            let baseDirCheck = false;
-            tempDirs.forEach((dirItem) => {
-                let newDirItem = dirItem.replace("modules/progress-clocks/themes/","");
-                if (dirItem.startsWith("modules/progress-clocks/themes/")) {
-                    newDirs.push(newDirItem);
-                    newPaths.push(dirItem);
-                    baseDirCheck = true;
-                }
-            });
-            if (!(baseDirCheck)) {
-                console.error("Failed.")
-            };
-            this._themes = newDirs;
-            this._themePaths = tempDirs;
-        }).catch(err => {
-            console.error(err)
-        });
         const isSupportedSize = size && Clock.sizes.indexOf(parseInt(size)) >= 0;
-        this._size = isSupportedSize ? parseInt(size) : Clock.sizes[0];
 
-        const p = (!progress || progress < 0) ? 0 : progress < this._size ? progress : this._size;
-        this._progress = p || 0;
+        this.size = isSupportedSize ? parseInt(size) : Clock.sizes[0];
 
-        this._theme = theme || this._themes?.[0] || "wallflower_green";
+        if (!progress || progress < 0) {
+            this.progress = 0;
+        } else if (progress > size) {
+            this.progress = this.size;
+        } else {
+            this.progress = progress;
+        }
+
+        this.theme = theme || 'wallflower_green';
     }
 
-    get theme () {
-        return this._theme;
+    get flags () {
+        return {
+            clocks: {
+                theme: this.theme,
+                size: this.size,
+                progress: this.progress
+            }
+        };
     }
 
-  get size () {
-    return this._size;
-  }
+    cycleSize () {
+        return new Clock({
+            theme: this.theme,
+            size: Clock.sizes[nextIndexInArray(Clock.sizes, this.size)],
+            progress: this.progress
+        });
+    }
 
-  get progress () {
-    return this._progress;
-  }
+    increment () {
+        const old = this;
+        return new Clock({
+            theme: old.theme,
+            size: old.size,
+            progress: old.progress + 1
+        });
+    }
 
-  get image () {
-    return { 
-      width: 350,
-      height: 350
-    };
-  }
+    decrement () {
+        const old = this;
+        return new Clock({
+            theme: old.theme,
+            size: old.size,
+            progress: old.progress - 1
+        });
+    }
 
-  get flags () {
-    return {
-      clocks: {
-        theme: this._theme,
-        size: this._size,
-        progress: this._progress
-      }
-    };
-  }
+    isEqual (clock) {
+        return clock
+            && clock.progress === this.progress
+            && clock.size === this.size
+            && clock.theme === this.theme;
+    }
 
-  cycleSize () {
-    return new Clock({
-      theme: this.theme,
-      size: Clock.sizes[nextIndexInArray(Clock.sizes, this.size)],
-      progress: this.progress
-    });
-  }
-
-  increment () {
-    const old = this;
-    return new Clock({
-      theme: old.theme,
-      size: old.size,
-      progress: old.progress + 1
-    });
-  }
-
-  decrement () {
-    const old = this;
-    return new Clock({
-      theme: old.theme,
-      size: old.size,
-      progress: old.progress - 1
-    });
-  }
-
-  isEqual (clock) {
-    return clock
-      && clock._progress === this._progress
-      && clock._size === this._size
-      && clock._theme === this._theme;
-  }
-
-  toString () {
-    return `${this._progress}/${this._size} • ${this._theme}`;
-  }
+    toString () {
+        return `${this.progress}/${this.size} • ${this.theme}`;
+    }
 }
--- module/scripts/sheet.js
+++ module/scripts/sheet.js
@@ -1,6 +1,5 @@
 import { Clock } from "./clock.js";
 import { getSystemMapping } from "./systems/index.js";
-import { log, warn } from "./util.js";
 
 const DISPLAY_NAME = {
     ALWAYS_FOR_EVERYONE: 50
@@ -18,10 +17,16 @@ const DEFAULT_TOKEN = {
 export class ClockSheet extends ActorSheet {
     static get defaultOptions() {
         const supportedSystem = getSystemMapping(game.data.system.id);
-	      return mergeObject(
+        return mergeObject(
             super.defaultOptions,
             {
-                classes: ["progress-clocks", "sheet", `progress-clocks-system-${game.data.system.id}`, "actor", "npc"],
+                classes: [
+                    "progress-clocks",
+                    "sheet",
+                    `progress-clocks-system-${game.data.system.id}`,
+                    "actor",
+                    "npc"
+                ],
                 template: "modules/progress-clocks/templates/sheet.html",
                 width: 375,
                 height: 600,
@@ -32,39 +37,40 @@ export class ClockSheet extends ActorSheet {
 
     static register () {
         const supportedSystem = getSystemMapping(game.data.system.id);
-        Actors.registerSheet(supportedSystem.id, ClockSheet, supportedSystem.registerSheetOptions);
-        log("Sheet Registered");
+        Actors.registerSheet(
+            supportedSystem.id,
+            ClockSheet,
+            supportedSystem.registerSheetOptions
+        );
     }
 
     constructor (...args) {
         super(...args);
-        this._system = getSystemMapping(game.data.system.id);
-    }
-
-    get system () {
-        return this._system;
+        this.system = getSystemMapping(game.data.system.id);
     }
 
     async getData () {
-        const clock = new Clock(this.system.loadClockFromActor({ actor: this.actor }));
-	      await clock.themesPromise;
-	      let themeDict = {};
-	      clock._themes.forEach((themeItem) =>{
-		        themeDict[themeItem] = clock._themePaths[clock._themes.indexOf(themeItem)]
-	      });
+        const clock = new Clock(
+            this.system.loadClockFromActor({
+                actor: this.actor
+            })
+        );
         return mergeObject(super.getData(), {
             clock: {
                 progress: clock.progress,
                 size: clock.size,
                 theme: clock.theme,
                 image: {
-                    url: `/${themeDict[clock.theme]}/${clock.size}clock_${clock.progress}.png`,
-                    width: clock.image.width,
-                    height: clock.image.height
+                    url: (
+                        `/${Clock.themes[clock.theme]}` +
+                        `/${clock.size}clock_${clock.progress}.png`
+                    ),
+                    width: Clock.image.width,
+                    height: Clock.image.height
                 },
                 settings: {
                     sizes: Clock.sizes,
-                    themes: clock._themes
+                    themes: Object.keys(Clock.themes)
                 }
             }
         });
@@ -73,26 +79,39 @@ export class ClockSheet extends ActorSheet {
     activateListeners (html) {
         super.activateListeners(html);
 
-        html.find("button[name=minus]").click(async (ev) => {
-            ev.preventDefault();
-            const oldClock = new Clock(this.system.loadClockFromActor({ actor: this.actor }));
+        html.find("button[name=minus]").click(async (e) => {
+            e.preventDefault();
+            const oldClock = new Clock(
+                this.system.loadClockFromActor({
+                    actor: this.actor
+                })
+            );
             this.updateClock(oldClock.decrement());
         });
 
-        html.find("button[name=plus]").click(async (ev) => {
-            ev.preventDefault();
-            const oldClock = new Clock(this.system.loadClockFromActor({ actor: this.actor }));
+        html.find("button[name=plus]").click(async (e) => {
+            e.preventDefault();
+            const oldClock = new Clock(
+                this.system.loadClockFromActor({
+                    actor: this.actor
+                })
+            );
             this.updateClock(oldClock.increment());
         });
 
         html.find("button[name=reset]").click(async (ev) => {
             ev.preventDefault();
-            const oldClock = new Clock(this.system.loadClockFromActor({ actor: this.actor }));
-            this.updateClock(new Clock({
+            const oldClock = new Clock(
+                this.system.loadClockFromActor({
+                    actor: this.actor
+                })
+            );
+            const newClock = new Clock({
                 theme: oldClock.theme,
                 progress: 0,
                 size: oldClock.size
-            }));
+            });
+            this.updateClock(newClock);
         });
     }
 
@@ -100,8 +119,11 @@ export class ClockSheet extends ActorSheet {
         await this.object.update({
             name: form.name
         });
-
-        const oldClock = new Clock(this.system.loadClockFromActor({ actor: this.actor }));
+        const oldClock = new Clock(
+            this.system.loadClockFromActor({
+                actor: this.actor
+            })
+        );
         let newClock = new Clock({
             progress: oldClock.progress,
             size: form.size,
@@ -113,41 +135,49 @@ export class ClockSheet extends ActorSheet {
     async updateClock(clock) {
         const actor = this.actor;
 
-	      await clock.themesPromise;
-
-	      let themeDict = {};
-	      clock._themes.forEach((themeItem) =>{
-		        themeDict[themeItem] = clock._themePaths[clock._themes.indexOf(themeItem)]
-	      });
-
 	      let fullVer = game.version ?? game.data.version;
+
         // update associated tokens
         const tokens = actor.getActiveTokens();
 	      let verMajor = fullVer.slice(0,3);
 
-        for (const t of tokens) {
+        for (const token of tokens) {
 		        //version check for compatability
 		        if (verMajor == "0.8" || verMajor.startsWith("9")) {
-			          await t.document.update({
+			          await token.document.update({
 				            name: actor.name,
-				            img: `/${themeDict[clock.theme]}/${clock.size}clock_${clock.progress}.png`,
+				            img: (
+                        `/${Clock.themes[clock.theme]}` +
+                        `/${clock.size}clock_${clock.progress}.png`
+                    ),
 				            actorLink: true
 			          });
 		        } else if (verMajor == "0.7"){
-			          await t.update({
+			          await token.update({
 				            name: actor.name,
-				            img: `/${themeDict[clock.theme]}/${clock.size}clock_${clock.progress}.png`,
+				            img: (
+                        `/${Clock.themes[clock.theme]}` +
+                        `/${clock.size}clock_${clock.progress}.png`
+                    ),
 				            actorLink: true
 			          });
 		        };
         }
 
         // update the Actor
-        const persistObj = await this.system.persistClockToActor({ actor, clock });
+        const persistObj = await this.system.persistClockToActor({
+            actor, clock
+        });
         const visualObj = {
-            img: `/${themeDict[clock.theme]}/${clock.size}clock_${clock.progress}.png`,
+				    img: (
+                `/${Clock.themes[clock.theme]}` +
+                `/${clock.size}clock_${clock.progress}.png`
+            ),
             token: {
-                img: `/${themeDict[clock.theme]}/${clock.size}clock_${clock.progress}.png`,
+				        img: (
+                    `/${Clock.themes[clock.theme]}` +
+                    `/${clock.size}clock_${clock.progress}.png`
+                ),
                 ...DEFAULT_TOKEN
             }
         };
