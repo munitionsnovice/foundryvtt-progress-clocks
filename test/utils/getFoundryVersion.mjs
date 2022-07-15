75ab79ddc4fcdefa6f01e02d84fe2bf2530b4636
Matt Dawson <matthewtdawson@gmail.com>
2022-07-14 22:10:47 -0400

    Fix test formatting

--- test/utils/getFoundryVersion.mjs
+++ test/utils/getFoundryVersion.mjs
@@ -3,21 +3,21 @@ import test from 'ava';
 import { getFoundryVersion } from "../../module/scripts/utils.mjs";
 
 const currentGame = {
-  version: "9.0"
+    version: "9.0"
 };
 
 const legacyGame = {
-  data: {
-    version: "0.7.1"
-  }
+    data: {
+        version: "0.7.1"
+    }
 };
 
 test("returns the correct version for a current game", t => {
-  const version = getFoundryVersion(currentGame);
-  t.deepEqual(version, { major: 9, minor: 0 });
+    const version = getFoundryVersion(currentGame);
+    t.deepEqual(version, { major: 9, minor: 0 });
 });
 
 test("returns the correct version for a legacy game", t => {
-  const version = getFoundryVersion(legacyGame);
-  t.deepEqual(version, { major: 7, minor: 1 });
+    const version = getFoundryVersion(legacyGame);
+    t.deepEqual(version, { major: 7, minor: 1 });
 });
