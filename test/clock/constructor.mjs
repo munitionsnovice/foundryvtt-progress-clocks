ba6964231b307eb2ce0fb0d876a4decba72e1a0f
Matt Dawson <matthewtdawson@gmail.com>
2022-07-18 16:29:25 -0400

    Add tests

--- test/clock/constructor.mjs
+++ test/clock/constructor.mjs
@@ -13,3 +13,17 @@ test("sets a default theme when none is provided", t => {
     const clock = new Clock();
     t.assert(clock.theme);
 });
+
+test("sets size to a supported size if supplied an unsupported size", t => {
+    const size = 1;
+    const clock = new Clock({
+        size: size
+    });
+    t.assert(Clock.sizes.indexOf(parseInt(size)) == -1);
+    t.assert(Clock.sizes.indexOf(parseInt(clock.size)) >= 0);
+});
+
+test("sets a default size when none is provided", t=> {
+    const clock = new Clock();
+    t.assert(clock.size);
+});
