80493aaa3630796e6d442068baec18860e62e6d3
Matt Dawson <mattdawson@WTMBP-Matt-Dawson.local>
2022-07-26 17:14:38 -0400

    Simplify actor loading and persisting

--- module/scripts/systems/dnd5e.mjs
+++ module/scripts/systems/dnd5e.mjs
@@ -1,13 +1,6 @@
 export default {
     registerSheetOptions: { types: ["npc"] },
 
-    loadClockFromActor: ({ actor }) => {
-        return {
-            progress: actor.getFlag("clocks", "progress"),
-            size: actor.getFlag("clocks", "size"),
-            theme: actor.getFlag("clocks", "theme")
-        };
-    },
 
     persistClockToActor: ({ clock }) => {
         return {
@@ -20,7 +13,7 @@ export default {
                 }
             },
             flags: {
-                clocks: {
+                "progress-clocks": {
                     progress: clock.progress,
                     size: clock.size,
                     theme: clock.theme,
